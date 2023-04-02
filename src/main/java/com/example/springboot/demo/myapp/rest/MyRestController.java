127ffc978752e31d2ea1cf319783f8b418acb985
Mohammed Rahman <minhaz357@gmail.com>
2023-04-02 07:12:39 -0400

    Define and inject custom properties

--- src/main/java/com/example/springboot/demo/myapp/rest/MyRestController.java
+++ src/main/java/com/example/springboot/demo/myapp/rest/MyRestController.java
@@ -1,27 +1,36 @@
 package com.example.springboot.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
 
+    // inject properties for: user.greeting, user.nickname, user.address
+    @Value("${user.greeting}")
+    private String userGreeting;
+    @Value("${user.nickname}")
+    private String userName;
+    @Value("${user.address}")
+    private String userAddress;
+
     // expose "/" that returns "Hello World"
     @GetMapping("/")
     public String sayHello(){
-        return "Hello World from MyRestController!";
+        return userGreeting;
     }
 
     // expose "/name" endpoint
     @GetMapping("/name")
     public String getName(){
-        return "Mohammed Rahman";
+        return userName;
     }
 
     // expose "/address" endpoint
     @GetMapping("/address")
     public String getAddress(){
-        return "New York, NY - 10017";
+        return userAddress;
     }
 
 

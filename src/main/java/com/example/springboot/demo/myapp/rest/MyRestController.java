f466ac7f7789a57f56552733fcc85e9c6def8169
Mohammed Rahman <minhaz357@gmail.com>
2023-04-08 06:43:35 -0400

    Add sayHello(param) method

--- src/main/java/com/example/springboot/demo/myapp/rest/MyRestController.java
+++ src/main/java/com/example/springboot/demo/myapp/rest/MyRestController.java
@@ -2,6 +2,7 @@ package com.example.springboot.demo.myapp.rest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +20,12 @@ public class MyRestController {
     @GetMapping("/")
     public String sayHello(){
         return userGreeting;
+    }
+
+    // expose the endpoint "/hello" and "/hello?name=John Doe"
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "MyWorld") String name){
+        return String.format("Hello %s!", name);
     }
 
     // expose "/name" endpoint

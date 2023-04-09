62490ab8f22ae40bf729892746b59acce635fc86
Mohammed Rahman <minhaz357@gmail.com>
2023-04-09 14:57:12 -0400

    Create Customer entity class

--- src/main/java/com/example/springboot/demo/myapp/MyappApplication.java
+++ src/main/java/com/example/springboot/demo/myapp/MyappApplication.java
@@ -1,14 +1,23 @@
 package com.example.springboot.demo.myapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MyappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){ // Java bean to run command line application
+		return runner ->{
+			System.out.println("Hello world");
+		};
 	}
 
 }
